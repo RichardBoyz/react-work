3310a94f05e4d7934e4bf9558c09785f5e12676f
richardboyz <r1382333@gmail.com>
2022-08-03 21:46:46 +0800

    Set test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import { getTestData } from "./apis/api";
 import "./App.css";
 
 function App() {
+  const [products, setProducts] = useState([]);
   async function getTest() {
     let res = await getTestData();
+    setProducts(res);
     console.log(res);
   }
+
   return (
     <div className="App" onClick={getTest}>
-      test
+      click me
+      {products.map((product) => (
+        <div key={product._id}>
+          <h2>title: {product.title}</h2>
+          <h2>description: {product.description}</h2>
+          <h2>price: {product.price}</h2>
+        </div>
+      ))}
     </div>
   );
 }
