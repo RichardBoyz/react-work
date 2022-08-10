e12b9a7857d7f3d20fcf883c64f2e8d170b4eb81
richardboyz <r1382333@gmail.com>
2022-08-10 18:26:19 +0800

    Remove test code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import { useState } from "react";
-import { getTestData } from "./apis/api";
 import "./App.css";
 
 function App() {
-  const [products, setProducts] = useState([]);
-  async function getTest() {
-    let res = await getTestData();
-    setProducts(res);
-    console.log(res);
-  }
-
-  return (
-    <div className="App" onClick={getTest}>
-      click me
-      {products.map((product) => (
-        <div key={product._id}>
-          <h2>title: {product.title}</h2>
-          <h2>description: {product.description}</h2>
-          <h2>price: {product.price}</h2>
-        </div>
-      ))}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
