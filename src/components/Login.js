f87a2e0d8f1df9324bf598f213521fadcf16e9bb
richardboyz <r1382333@gmail.com>
2022-08-10 18:29:10 +0800

    Set login basic hook params

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const Login = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [success, setSuccess] = useState(false);
+
   return <div>Login</div>;
 };
 
